df14c574afac636c520a4b0a5e674b69e588144f
Mackintosh <simonib20@gmail.com>
2025-06-03 19:35:31 +0200

    fix for ci/cd

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -97,10 +97,10 @@ resource "azurerm_databricks_workspace" "databricks" {
   managed_resource_group_name = "${azurerm_resource_group.rg.name}-managed"
 
   custom_parameters {
-    no_public_ip        = true
-    virtual_network_id  = azurerm_virtual_network.vnet.id
-    public_subnet_name  = azurerm_subnet.public.name
-    private_subnet_name = azurerm_subnet.private.name
+    no_public_ip                                         = true
+    virtual_network_id                                   = azurerm_virtual_network.vnet.id
+    public_subnet_name                                   = azurerm_subnet.public.name
+    private_subnet_name                                  = azurerm_subnet.private.name
     public_subnet_network_security_group_association_id  = azurerm_subnet_network_security_group_association.public.id
     private_subnet_network_security_group_association_id = azurerm_subnet_network_security_group_association.private.id
   }
